1175b400a270d9be42249594a96dd156f75fdb27
Dan Arpoika <danarpoika@gmail.com>
2025-09-29 12:44:40 -0400

    add: local ssl cert for https

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,7 +5,20 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
+  preview: {
+    port: 3001,
+    https: {
+      cert: path.resolve(__dirname, "../../.cert/cert.pem"),
+      key: path.resolve(__dirname, "../../.cert/key.pem"),
+    },
+  },
   server: {
+    port: 3001,
+    https: {
+      cert: path.resolve(__dirname, "../../.cert/cert.pem"),
+      key: path.resolve(__dirname, "../../.cert/key.pem"),
+    },
+    allowedHosts: ["localhost"],
     proxy: {
       "/api": {
         target: "http://localhost:3000",
